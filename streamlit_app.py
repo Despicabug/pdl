1d9753e424d7ab1af1c19e611883471202bae9ee
despicabug <vjamwal2000@gmail.com>
2023-01-04 15:09:35 +0530

    whisper1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,23 @@
 import streamlit as st
-from pydub import AudioSegment, silence
-import speech_recognition as sr
-import os
-recog=sr.Recognizer()
-final_result=""
-st.markdown("<h1 style='text-align: center;'>Audio To Text Converter</h1>", unsafe_allow_html=True)
-st.markdown("---", unsafe_allow_html=True)
-audio=st.file_uploader("Upload Your Audio File", type=['mp3', 'wav'])
-if audio:
-    st.audio(audio)
-    audio_segment=AudioSegment.from_file(audio)
-    chunks=silence.split_on_silence(audio_segment, min_silence_len=500, silence_thresh=audio_segment.dBFS-20, keep_silence=100)
-    for index,chunk in enumerate(chunks):
-        chunk.export(str(index)+".wav", format="wav")
-        with sr.AudioFile(str(index)+".wav") as source:
-            recorded=recog.record(source)
-            try:
-                text=recog.recognize_google(recorded)
-                final_result=final_result+" "+text
-                print(text)
-            except:
-                print("None")
-                final_result=final_result+" Unaudible"
-    with st.form("Result"): 
-        result=st.text_area("TEXT", value=final_result) 
-        d_btn=st.form_submit_button("Download")    
-        if d_btn:
-            envir_var=os.environ
-            usr_loc=envir_var.get('USERPROFILE')
-            loc=usr_loc+"\Downloads\\transcript.txt"
-            with open(loc, 'w') as file:
-                file.write(result)
+import whisper
+
+st.title("Transcriber")
+
+audio_file = st.file_uploader("Upload Audio",type=["wav","mp3","m4a"])
+
+model = whisper.load_model("base")
+st.text("Whisper Model Loaded")
+
+if st.sidebar.button("Transcribe Audio"):
+    if audio_file is not None:
+        st.sidebar.success("Transcribing Audio")
+        transcription = model.transcribe(audio_file.name)
+        st.sidebar.success("Transcription Complete")
+        st.markdown(transcription["text"])
+    else:
+        st.sidebar.error("Please upload an audio file")
+
+st.sidebar.header("Play Original Audio File")
+st.sidebar.audio(audio_file)
+
         
